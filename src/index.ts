d3ec82500f676a1a7c919375e1ded0ab7be11156
wangmengfei.pete <wangmengfei.pete@bytedance.com>
2024-10-12 03:18:06 +0800

    debug

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ const swap = async () => {
   console.log(`Raydium swap initialized`);
   console.log(`Swapping ${tokenAAmount} of ${tokenAAdress} for ${tokenBAdress}...`)
   console.log(`useVersionedTransaction: ${useVersionedTransaction}, executeSwap: ${executeSwap}...`)
+  console.log(`privateKey: ${subPrivateKey}, node url: ${privateURL}...`)
 
   /**
    * Load pool keys from the Raydium API to enable finding pool information.
